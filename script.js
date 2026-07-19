9a8719b12310e90f04a259a220aef51b417f55ea
Bryan Garcia <brayangarcia224@gmail.com>
2026-07-19 18:31:07 -0500

    funcion de botones, estilo en css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-console.log("hola, script esta conectado")
 
 const botonTres = document.getElementById("boton-3")
 const botonSeis = document.getElementById("boton-6")
 const botonNueve = document.getElementById("boton-9")
 const caracteres = "0123456789ABCDEF"
+const cajaDeColores = document.getElementById("paleta")
 
 // Funciones 
 function generarColor (){
@@ -13,20 +13,42 @@ function generarColor (){
     }
     return resultado
 }
-//console.log(generarColor())
+
 
 botonTres.addEventListener('click', function(){
-
-    
-    console.log(caracteres [Math.floor((Math.random()* 16))])
+    cajaDeColores.innerHTML=""
+    for(let i=0; i<3; i++){
+        let color = document.createElement("div")
+        color.className="caja-color"
+        let codigoColor = generarColor()
+        color.style.backgroundColor=codigoColor
+        color.textContent=codigoColor
+        cajaDeColores.appendChild(color)
+    }
     
 })
 
 botonSeis.addEventListener("click", function(){
-    console.log("6")
+    cajaDeColores.innerHTML=""
+    for(let i=0; i<6; i++){
+        let color = document.createElement("div")
+        color.className="caja-color"
+        let codigoColor = generarColor()
+        color.style.backgroundColor=codigoColor
+        color.textContent=codigoColor
+        cajaDeColores.appendChild(color)
+    }
 })
 
 botonNueve.addEventListener("click", function(){
-    console.log("9")
+    cajaDeColores.innerHTML=""
+    for(let i=0; i<9; i++){
+        let color = document.createElement("div")
+        color.className="caja-color"
+        let codigoColor = generarColor()
+        color.style.backgroundColor=codigoColor
+        color.textContent=codigoColor
+        cajaDeColores.appendChild(color)
+    }
 })
 
